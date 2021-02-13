caf4b6c763d17c501c8316ca6cdf68845d568726
Jelmer Kuijpers <jelmerkuypers@hotmail.com>
2021-02-13 14:51:45 +0100

    Update app.vue

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,18 +1,35 @@
 <template>
-    <div>
-        {{message}}
+    <div class="taskListContainer">
+        <div class="heading">
+            <h2 id="title">Task list</h2>
+            <add-task-form></add-task-form>
+        </div>
+        <list-view></list-view>
     </div>
 </template>
 <script>
-const default_layout = "default";
-
-
+import addTaskForm from "./addTaskForm";
+import listView from "./listView";
 export default {
-    computed: {},
-    data() {
-        return {
-            message:'Hello World'
-        }
+    components: {
+        addTaskForm,
+        listView
     }
-};
+}
 </script>
+
+<style>
+.taskListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading {
+    background: white;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+</style>
