0e6922b74c1245702818185f12216f7ce60e09de
Jelmer Kuijpers <jelmerkuypers@hotmail.com>
2021-02-13 09:41:14 +0100

    Wrote Task CRUD tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -14,13 +15,69 @@ class TaskTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $data = [
+        $taskData = [
             'name' => 'Test task',
             'completed' => false
         ];
 
-        $this->postJson(route('task.store'), $data)
+        $this->postJson(route('task.store'), $taskData)
             ->assertCreated();
-        $this->assertDatabaseHas('tasks', $data);
+        $this->assertDatabaseHas('tasks', $taskData);
+    }
+
+    /** @test */
+    public function it_can_edit_a_task()
+    {
+        $this->withoutExceptionHandling();
+
+        $task = Task::factory()->create();
+        $taskData = [
+            'name' => 'Test task',
+        ];
+
+        $this->putJson(route('task.store', $task), $taskData)
+            ->assertOk();
+        $this->assertDatabaseHas('tasks', $taskData);
+    }
+
+    /** @test */
+    public function it_can_delete_a_task()
+    {
+        $this->withoutExceptionHandling();
+
+        $task = Task::factory()->create();
+
+        $this->deleteJson('tasks.delete', $task)
+            ->assertOk();
+        $this->assertDatabaseMissing('tasks', $task);
+    }
+
+    /** @test */
+    public function it_can_toggle_a_task_its_status()
+    {
+        $this->withoutExceptionHandling();
+
+        $task = Task::factory()->create([
+            'completed' => false
+        ]);
+
+        $this->putJson(route('task.toggle.complete', $task), [
+            'completed' => true
+        ])->assertOk();
+
+        $this->assertDatabaseHas('tasks', [
+            'name' => $task['name'],
+            'completed' => true
+        ]);
+    }
+
+    /** @test */
+    public function it_can_show_all_tasks()
+    {
+        $this->withoutExceptionHandling();
+
+        $tasks = Task::factory()->count(5)->create();
+
+        $this->getJson(route('task.index'))->assertJson($tasks);
     }
 }
