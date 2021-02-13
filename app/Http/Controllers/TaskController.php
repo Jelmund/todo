9fd2beceeeec41c809939933aac1e88709801d0d
Jelmer Kuijpers <jelmerkuypers@hotmail.com>
2021-02-13 12:32:11 +0100

    Finish index and store methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Task\StoreTaskRequest;
+use Illuminate\Support\Collection;
+
+use App\Models\Task;
+
 
 class TaskController extends Controller
 {
-    //
+    public function index(): Collection
+    {
+        return Task::all();
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $attributes = $request->validated();
+
+        Task::create($attributes);
+    }
 }
